3aebc5f9be90b63afdc748ce7c59e5dd79e5ea5e
«Artur» <art13471118@gmail.com>
2022-12-07 20:06:33 +0300

    Привводе четверти выводит возможное значение координат xy

---
+++ Task18/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает дипозон возможных координат точек в этой четверти (X и Y)
+
+Console.WriteLine("Введите номер четверти");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num == 1) Console.WriteLine("X>0 && Y>0");
+if (num == 2) Console.WriteLine("X<0 && Y>0");
+if (num == 3) Console.WriteLine("X<0 && Y<0");
+if (num == 4) Console.WriteLine("X>0 && Y<0");
