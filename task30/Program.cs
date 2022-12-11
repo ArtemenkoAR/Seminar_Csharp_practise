003539b9f7d39c3ea947b1f96506269f84be0e7a
«Artur» <art13471118@gmail.com>
2022-12-11 23:49:10 +0300

    Программа выдает массив из 8 чисел, заполняя рандомно от 0 до 1

---
+++ task30/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+void Array1(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+
+    // int i = 0;
+    // while (i < array.Length)
+    // {
+    //     array[i] = new Random().Next(0, 2);
+    // }
+    // i++;
+}
+void Array2(int[] print)
+{
+    for (int i = 0; i < print.Length; i++)
+    {
+        Console.Write($"{print[i]} ");
+    }
+    // int i = 0;
+    // while (i < print.Length)
+    // {
+    //     Console.WriteLine(print[i]);
+    //     i++;
+    // }
+}
+
+int[] array = new int[8];
+Array1(array);
+Array2(array);
