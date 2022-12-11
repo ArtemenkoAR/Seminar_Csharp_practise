175d730fd72eb0e6947a3d78940d85d9c7415ffe
«Artur» <art13471118@gmail.com>
2022-12-11 22:53:09 +0300

    Программа принимает число n и указывает произведение всех чисел от 1 до n

---
+++ task28/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число N");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Произведение всех чисел от 1 до числа N равно = {Product(num)}");
+
+int Product(int A)
+{
+    int i = 1;
+    int b = 1;
+    while (i <= A)
+    {
+        b *= i;
+        i++;
+    }
+    return b;
+}
