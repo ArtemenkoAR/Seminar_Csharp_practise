2fc05e5529536aa299ba0f89a8182f0875ad7b96
«Artur» <art13471118@gmail.com>
2022-12-11 22:21:21 +0300

    Программа принимает число и указывает количество цифр в этом числе через функцию

---
+++ task26/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Количество цифр в числе равно = {CountNum(num)}");
+
+int CountNum(int A)
+{
+    int count = 0;
+
+    while (A > 0)
+    {
+        count++;
+        A = A / 10;
+    }
+    return count;
+}
