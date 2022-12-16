96c2f9630583a82b5da52d5567ff81c0f2486de7
«Artur» <art13471118@gmail.com>
2022-12-16 20:28:24 +0300

    Программа, где задается массив исимволы меняются местами

---
+++ Task39/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.WriteLine("Hello, World!");
+int[] array = GetArray(9, 0, 10);
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine();
+int[] reversArray = ReversArray2(array);
+//ReversArray1(array);
+Console.WriteLine(string.Join(" ", reversArray));
+
+int[] GetArray(int size, int minValue, int maxvalue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxvalue + 1);
+    }
+    return res;
+}
+
+void ReversArray1(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length / 2; i++)
+    {
+        int k = inArray[i];
+        inArray[i] = inArray[inArray.Length - 1 - i];
+        inArray[inArray.Length - 1 - i] = k;
+    }
+}
+
+int[] ReversArray2(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[inArray.Length - 1 - i];
+    }
+    return result;
+}
